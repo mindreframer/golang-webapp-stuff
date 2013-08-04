d6026a1f94fb5e439e217e4a32a452dc42bfcb1c
Roman Heinrich <roman.heinrich@gmail.com>
2013-08-04 17:32:18 +0200

    Squashed 'src/github.com/emicklei/mora/' content from commit 045d84a
    git-subtree-dir: src/github.com/emicklei/mora
    git-subtree-split: 045d84a64b02a94cd7437dd7a168fb4ae9697f2c

---
+++ doc.go
@@ -0,0 +1,16 @@
+/*
+Command mora - MongoDB Rest API, a generic REST server in Go for accessing MongoDB documents and meta data
+
+	http://localhost:8181/docs/local/landskape/connections/51caec2e95c51cb63a584fde
+
+returns the document from
+
+ local=name of alias for mongodb hosted on localhost as defined in configuration file
+ database=landskape
+ collection=connections
+ _id=51caec2e95c51cb63a584fde
+
+
+See README for full API.
+*/
+package main
