3ed8711a64a2dcffad2081b2a2aa8e3f0d77ef68
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-07 21:02:02 +0200

    Squashed 'src/github.com/stretchr/goweb/' content from commit 1cdfe4e
    git-subtree-dir: src/github.com/stretchr/goweb
    git-subtree-split: 1cdfe4e7ab3abc936bca2be24798864f455c5463

---
+++ example_webapp/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/stretchr/codecs/services"
+	"github.com/stretchr/goweb"
+	"github.com/stretchr/goweb/handlers"
+	"github.com/stretchr/testify/assert"
+	testifyhttp "github.com/stretchr/testify/http"
+	"net/http"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+
+	// make a test HttpHandler and use it
+	codecService := new(services.WebCodecService)
+	handler := handlers.NewHttpHandler(codecService)
+	goweb.SetDefaultHttpHandler(handler)
+
+	// call the target code
+	mapRoutes()
+
+	goweb.Test(t, "GET people/me", func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+
+		// should be a redirect
+		assert.Equal(t, http.StatusFound, response.WrittenHeaderInt, "Status code should be correct")
+
+	})
+
+}
