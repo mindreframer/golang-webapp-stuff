614b6fb9af3a98166cd641d7e4c3b16cb986b2c8
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-08 12:26:12 +0200

    Squashed 'src/github.com/kentaro/delta/' content from commit a79b48f
    git-subtree-dir: src/github.com/kentaro/delta
    git-subtree-split: a79b48ffcec53a196b358dcade3e712c4372c6fb

---
+++ response.go
@@ -0,0 +1,29 @@
+package delta
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Response struct {
+	Backend      *Backend
+	HttpResponse *http.Response
+	Data         []byte
+	Elapsed      time.Duration
+}
+
+func NewResponse(backend *Backend, httpResponse *http.Response, elapsed time.Duration) (*Response, error) {
+	response := &Response{
+		Backend:      backend,
+		HttpResponse: httpResponse,
+		Data:         make([]byte, 0),
+		Elapsed:      elapsed,
+	}
+	data, err := ioutil.ReadAll(httpResponse.Body)
+
+	response.HttpResponse.Body.Close()
+	response.Data = data
+
+	return response, err
+}
---
+++ server.go
@@ -0,0 +1,82 @@
+// Package delta is an HTTP shadow proxy server that sits between
+// clients and your server(s) to enable "shadow requests".
+package delta
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+)
+
+type Server struct {
+	Host     string
+	Port     int
+	Backends map[string]*Backend
+
+	onSelecBackendtHandler   func(req *http.Request) []string
+	onMungeHeaderHandler     func(backend string, header *http.Header)
+	onBackendFinishedHandler func(map[string]*Response)
+}
+
+func NewServer(host string, port int) *Server {
+	server := &Server{
+		Host: host,
+		Port: port,
+	}
+	server.Backends = make(map[string]*Backend)
+
+	// By default, all backends will be selected
+	server.OnSelectBackend(func(req *http.Request) []string {
+		backends := make([]string, 0)
+
+		for key, _ := range server.Backends {
+			backends = append(backends, key)
+		}
+
+		return backends
+	})
+
+	return server
+}
+
+func (server *Server) AddMasterBackend(name, host string, port int) {
+	server.Backends[name] = &Backend{
+		IsMaster: true,
+		Name:     name,
+		Host:     host,
+		Port:     port,
+	}
+}
+
+func (server *Server) AddBackend(name, host string, port int) {
+	server.Backends[name] = &Backend{
+		IsMaster: false,
+		Name:     name,
+		Host:     host,
+		Port:     port,
+	}
+}
+
+func (server *Server) OnSelectBackend(handler func(req *http.Request) []string) {
+	server.onSelecBackendtHandler = handler
+}
+
+func (server *Server) OnMungeHeader(handler func(backend string, header *http.Header)) {
+	server.onMungeHeaderHandler = handler
+}
+
+func (server *Server) OnBackendFinished(handler func(responses map[string]*Response)) {
+	server.onBackendFinishedHandler = handler
+}
+
+func (server *Server) Run() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.Host, server.Port))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", NewHandler(server))
+	log.Fatal(http.Serve(listener, nil))
+}
